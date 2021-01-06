89ca7d6da42bbedc1b59857f82efb54f4ba8e6d8
pkoniu <patryk.konior@gmail.com>
2021-01-06 18:00:14 +0100

    test

--- jlupin-platform-intro/scripts/install-jlupin.sh
+++ jlupin-platform-intro/scripts/install-jlupin.sh
@@ -13,15 +13,15 @@ unzip jlupin.zip -d /opt/jlupin
 chmod 750 /opt/jlupin/platform/start/start.sh
 chmod 750 /opt/jlupin/platform/start/control.sh
 echo 'user root root;' | cat - /opt/jlupin/platform/start/configuration/edge.conf | tee /opt/jlupin/platform/start/configuration/edge.conf
-cd /opt/jlupin/platform/application
-for f in $(find . -name configuration.yml); do sed -i 's/Xmx[0-9]*M/Xmx64M/g' $f; done
-for f in $(find . -name configuration.yml); do sed -i 's/Xms[0-9]*M/Xms64M/g' $f; done
-for f in $(find . -name servlet_configuration.yml); do sed -i 's/Xmx[0-9]*M/Xmx100M/g' $f; done
-for f in $(find . -name servlet_configuration.yml); do sed -i 's/Xms[0-9]*M/Xms100M/g' $f; done
-sed -i 's/Xmx[0-9]*M/Xmx64M/g' /opt/jlupin/platform/start/configuration/setenv
-sed -i 's/Xms[0-9]*M/Xms64M/g' /opt/jlupin/platform/start/configuration/setenv
-sed -i 's/Xms[0-9]*M/Xms192M/g' /opt/jlupin/platform/application/webcontrol/servlet_configuration.yml
-sed -i 's/Xmx[0-9]*M/Xmx192M/g' /opt/jlupin/platform/application/webcontrol/servlet_configuration.yml
+# cd /opt/jlupin/platform/application
+# for f in $(find . -name configuration.yml); do sed -i 's/Xmx[0-9]*M/Xmx64M/g' $f; done
+# for f in $(find . -name configuration.yml); do sed -i 's/Xms[0-9]*M/Xms64M/g' $f; done
+# for f in $(find . -name servlet_configuration.yml); do sed -i 's/Xmx[0-9]*M/Xmx100M/g' $f; done
+# for f in $(find . -name servlet_configuration.yml); do sed -i 's/Xms[0-9]*M/Xms100M/g' $f; done
+# sed -i 's/Xmx[0-9]*M/Xmx64M/g' /opt/jlupin/platform/start/configuration/setenv
+# sed -i 's/Xms[0-9]*M/Xms64M/g' /opt/jlupin/platform/start/configuration/setenv
+# sed -i 's/Xms[0-9]*M/Xms192M/g' /opt/jlupin/platform/application/webcontrol/servlet_configuration.yml
+# sed -i 's/Xmx[0-9]*M/Xmx192M/g' /opt/jlupin/platform/application/webcontrol/servlet_configuration.yml
 
 # echo "Disabling SSL"
 cd /opt/jlupin/platform
