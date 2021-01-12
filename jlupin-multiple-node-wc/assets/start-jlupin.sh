2dd22f0b84fe6be224588d6e98939d1da4eb2905
pkoniu <patryk.konior@gmail.com>
2021-01-12 12:57:13 +0100

    test

--- jlupin-multiple-node-wc/assets/start-jlupin.sh
+++ jlupin-multiple-node-wc/assets/start-jlupin.sh
@@ -6,11 +6,12 @@ echo "Starting JLupin platform..."
 
 /opt/jlupin/platform/start/start.sh
 
+echo "Waiting for intialization..."
+
 status=$(curl -w "%{http_code}\\n" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'Connection: keep-alive' --data-raw $'{\n  "value": "12",\n  "currency": "USD"\n}' http://localhost:8000/exchange/convert -s -o /dev/null)
 
 while [[ "$status" != "200" ]]
 do
-  echo "Current status: $status"
   sleep 5
   status=$(curl -w "%{http_code}\\n" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'Connection: keep-alive' --data-raw $'{\n  "value": "12",\n  "currency": "USD"\n}' http://localhost:8000/exchange/convert -s -o /dev/null)
 done
