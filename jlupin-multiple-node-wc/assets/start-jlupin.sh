3d890bf7478329edfd4bd6c91b407bee86eae863
pkoniu <patryk.konior@gmail.com>
2021-01-12 14:15:21 +0100

    test

--- jlupin-multiple-node-wc/assets/start-jlupin.sh
+++ jlupin-multiple-node-wc/assets/start-jlupin.sh
@@ -6,27 +6,19 @@ echo "Starting JLupin platform for node $node_number..."
 
 /opt/jlupin/platform${node_number}/start/start.sh
 
-echo "Waiting for intialization..."
-
 status=$(curl -w "%{http_code}\\n" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'Connection: keep-alive' --data-raw $'{\n  "value": "12",\n  "currency": "USD"\n}' http://localhost:8000/exchange/convert -s -o /dev/null)
 
 check-jlupin-status.sh &
-
 pid=$!
 
-spin[0]="-"
-spin[1]="\\"
-spin[2]="|"
-spin[3]="/"
-
-echo -n "[initializing] ${spin[0]}"
-while [ kill -0 $pid ]
+echo "Waiting for initialization..."
+spin='-\|/'
+i=0
+while kill -0 $pid 2>/dev/null
 do
-  for i in "${spin[@]}"
-  do
-        echo -ne "\b$i"
-        sleep 0.1
-  done
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
 done
 
 echo "JLupin platform started successfully."
---
+++ test.sh
@@ -0,0 +1,14 @@
+./proc.sh &
+
+pid=$!
+
+spin='-\|/'
+
+echo "Waiting..."
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
