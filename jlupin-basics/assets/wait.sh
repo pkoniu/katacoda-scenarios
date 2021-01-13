db529e72dc7f0f8a8bcee8247296908e2989824b
pkoniu <patryk.konior@gmail.com>
2021-01-13 14:07:18 +0100

    test

--- jlupin-basics/assets/wait.sh
+++ jlupin-basics/assets/wait.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-spin_until()
+show_progress()
 {
-  notification_file=$1
+  local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
+
+  echo -n "Installing system dependencies..."
   while true; do 
-    sudo grep -i "done" notification_file &> /dev/null
+    sudo grep -i "done" /opt/.sys-deps-installed &> /dev/null
     if [[ "$?" -ne 0 ]]; then     
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -21,26 +23,74 @@ spin_until()
   printf "    \b\b\b\b"
   echo ""
   echo "Done!"
-}
-
-show_progress()
-{
-  local -r pid="${1}"
-
-  echo -n "Installing system dependencies..."
-  spin_until /opt/.sys-deps-installed
 
   echo -n "Downloading jlupin@1.6.1..."
-  spin_until /opt/.jlupin-downloaded
+  while true; do 
+    sudo grep -i "done" /opt/.jlupin-downloaded &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Done!"
 
   echo -n "Preparing jlupin configuration..."
-  spin_until /opt/.jlupin-setup
+  while true; do 
+    sudo grep -i "done" /opt/.jlupin-setup &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Done!"
 
   echo -n "Starting jlupin platform..."
-  spin_until /opt/.jlupin-started
+  while true; do 
+    sudo grep -i "done" /opt/.jlupin-started &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Done!"
 
   echo -n "Waiting for example app to be available..."
-  spin_until /opt/.exchange-available
+  while true; do 
+    sudo grep -i "done" /opt/.exchange-available &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Done!"
 }
 
 show_progress
