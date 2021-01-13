44532a7b116ba794875c8cc90ef384478f6e2238
pkoniu <patryk.konior@gmail.com>
2021-01-13 13:50:56 +0100

    test

---
+++ jlupin-basics/scripts/background.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# echo "Installing system dependencies"
+apt update && apt install -y curl unzip htop
+echo "done" >> /opt/.sys-deps-installed
+
+# echo "Downloading jlupin@1.6.1"
+curl https://kacdab-download.s3.eu-central-1.amazonaws.com/jlupin_platform_version_1_6_1_latest.zip -o jlupin.zip
+echo "done" >> /opt/.jlupin-downloaded
+
+# echo "Preparing JLupin"
+mkdir -p /opt/jlupin
+unzip jlupin.zip -d /opt/jlupin
+chmod 750 /opt/jlupin/platform/start/start.sh
+chmod 750 /opt/jlupin/platform/start/control.sh
+sed -i '1iuser root root;' /opt/jlupin/platform/start/configuration/edge.conf
+sed -i '/ssl/ s/^#*/#/g' /opt/jlupin/platform/technical/nginx/linux/conf/servers/admin.conf
+echo "done" >> /opt/.jlupin-setup
+
+# echo "Starting JLupin platform"
+/opt/jlupin/platform/start/start.sh
+
+status=$(curl -w "%{http_code}\\n" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'Connection: keep-alive' --data-raw $'{\n  "value": "12",\n  "currency": "USD"\n}' http://localhost:8000/exchange/convert -s -o /dev/null)
+
+while [[ "$status" != "200" ]]
+do
+  sleep 5
+  status=$(curl -w "%{http_code}\\n" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'Connection: keep-alive' --data-raw $'{\n  "value": "12",\n  "currency": "USD"\n}' http://localhost:8000/exchange/convert -s -o /dev/null)
+done
+
+# echo "Finished"
+echo "done" >> /opt/.jlupin-started
--- jlupin-basics/scripts/wait-for-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Tweaking system settings"; while [ ! -f /opt/.sys-setup ] ; do sleep 2; done; echo "Done!"
-echo "Installing system dependencies"; while [ ! -f /opt/.sys-dep-install ] ; do sleep 2; done; echo "Done!"
-echo "Downloading jlupin"; while [ ! -f /opt/.jlupin-download ] ; do sleep 2; done; echo "Done!"
-echo "Setting up jlupin"; while [ ! -f /opt/.jlupin-setup ] ; do sleep 2; done; echo "Setup finished!"
-echo "Starting jlupin node with example application"; while [ ! -f /opt/.scenario-setup ] ; do sleep 2; done; echo "Setup finished!"
