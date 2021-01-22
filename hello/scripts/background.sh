b9e8a5e1fb4cfd8e1c532b5211b330ecaeea7e0d
pkoniu <patryk.konior@gmail.com>
2021-01-22 15:16:38 +0100

    test

--- hello/scripts/background.sh
+++ hello/scripts/background.sh
@@ -43,5 +43,5 @@ echo "done" >> /opt/.jlupin-prepared
 
 # echo "Preparing project structure"
 mkdir -p /root/scenario/project
-tar -zxvf /root/scenario/hello-jlupin-empty.tgz -C /root/scenario/project/
+unzip /root/scenario/hello-jlupin.zip -d /root/scenario/project/
 echo "done" >> /opt/.project-prepared
